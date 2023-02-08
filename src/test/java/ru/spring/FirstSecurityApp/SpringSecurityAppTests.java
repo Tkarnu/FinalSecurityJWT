8e423ec33264bc43527c6a50bf81fa0135f61a43
Sergey Kravets <102000592+Tkarnu@users.noreply.github.com>
2023-02-08 15:23:40 +0300

    ready to start adding jwt

---
+++ src/test/java/ru/spring/FirstSecurityApp/SpringSecurityAppTests.java
@@ -0,0 +1,13 @@
+package ru.spring.FirstSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
